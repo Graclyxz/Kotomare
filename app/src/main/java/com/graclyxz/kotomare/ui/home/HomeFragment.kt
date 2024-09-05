5ab35015e67f756e92b502f430404ce7c211860f
Xavier Villavicencio <jxavier2525@gmail.com>
2024-09-04 23:19:48 -0500

    AnimeFLV ADS lockout and full screen in videos

--- app/src/main/java/com/graclyxz/kotomare/ui/home/HomeFragment.kt
+++ app/src/main/java/com/graclyxz/kotomare/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.graclyxz.kotomare.ui.home
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +9,11 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.TextView
+import android.widget.FrameLayout
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+//import androidx.lifecycle.ViewModelProvider
 import com.graclyxz.kotomare.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -29,8 +29,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+       // val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -40,7 +39,47 @@ class HomeFragment : Fragment() {
             textView.text = it
         }*/
 
-        binding.webViewFLV.webChromeClient = WebChromeClient()
+        binding.webViewFLV.webChromeClient = object : WebChromeClient() {
+            private var customView: View? = null
+            private var customViewCallback: CustomViewCallback? = null
+            private var originalSystemUiVisibility: Int = 0
+
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                if (customView != null) {
+                    onHideCustomView()
+                    return
+                }
+
+                customView = view
+                originalSystemUiVisibility = requireActivity().window.decorView.systemUiVisibility
+                customViewCallback = callback
+
+                // Configurar vista en pantalla completa
+                val decorView = requireActivity().window.decorView as FrameLayout
+                decorView.addView(customView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
+                requireActivity().window.decorView.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+                // Ocultar ActionBar en pantalla completa
+                (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+            }
+
+            override fun onHideCustomView() {
+                val decorView = requireActivity().window.decorView as FrameLayout
+                decorView.removeView(customView)
+                customView = null
+                requireActivity().window.decorView.systemUiVisibility = originalSystemUiVisibility
+
+                // Mostrar ActionBar al salir de pantalla completa
+                (requireActivity() as AppCompatActivity).supportActionBar?.show()
+
+                customViewCallback?.onCustomViewHidden()
+                customViewCallback = null
+            }
+        }
+
 
         binding.webViewFLV.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
@@ -54,7 +93,10 @@ class HomeFragment : Fragment() {
                 val url = request?.url.toString()
 
                 // Lista de dominios conocidos de anuncios
-                val adDomains = listOf("ak.zougreek.com", "ak.hesoorda.com")
+                val adDomains = listOf("ak.zougreek.com", "ak.hesoorda.com", "s.optnx.com",
+                    "continuedownloader.com", "my.mail.ru", "ig.caudataolibene.com",
+                    "onclickperformance.com", "ak.omgothitar.com", "ig.caudataolibene.com",
+                    "akutapro.com", "glersakr.com")
 
                 // Si la URL contiene un dominio de anuncio, bloquea la solicitud
                 if (adDomains.any { url.contains(it) }) {
@@ -66,12 +108,15 @@ class HomeFragment : Fragment() {
                 return super.shouldInterceptRequest(view, request)
             }
         }
+
+        // AnimeFLV settings
         binding.webViewFLV.settings.javaScriptEnabled = true
         binding.webViewFLV.settings.domStorageEnabled = true
         binding.webViewFLV.settings.mediaPlaybackRequiresUserGesture = false
         binding.webViewFLV.settings.loadWithOverviewMode = true
         binding.webViewFLV.settings.useWideViewPort = true
 
+        // Load AnimeFLV URL
         binding.webViewFLV.loadUrl("https://www3.animeflv.net")
 
         return root
@@ -101,5 +146,4 @@ class HomeFragment : Fragment() {
             }
         )
     }
-
 }
